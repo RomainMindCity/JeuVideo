6b046323e144e7e5fd657a6c3be10b11172a52e6
RomainMindCity <fun.gaming72200@gmail.com>
2022-10-21 11:43:31 +0200

    Leveling System : Optimisation Skills

--- Assets/Scripts/Leveling System/LevelingSystem.cs
+++ Assets/Scripts/Leveling System/LevelingSystem.cs
@@ -5,62 +5,15 @@ using UnityEngine;
 public class LevelingSystem : MonoBehaviour
 {
 
-    struct LevelFormat
-    {
-        public string Name;
-        public string Type;
-        public int Level;
-        public int CurrentExperiance;
-        public int ToNextExperiance;
-        public int MaxLevel;      
-    }
-
-    public class Bucheron
-    {
-        public static void Main(string[] args)
-        {
-            LevelFormat Lumberjack;
-            LevelFormat Miner;
-            LevelFormat Hunter;
-            LevelFormat FisherMan;
-            LevelFormat Builder;
-
-            Lumberjack.Name = "Lumberjack";
-            Lumberjack.Type = "Craftra";
-            Lumberjack.Level = 1;
-            Lumberjack.CurrentExperiance = 0;
-            Lumberjack.ToNextExperiance = 1280;
-            Lumberjack.MaxLevel = 50;
-
-            Miner.Name = "Miner";
-            Miner.Type = "Craftra";
-            Miner.Level = 1;
-            Miner.CurrentExperiance = 0;
-            Miner.ToNextExperiance = 1280;
-            Miner.MaxLevel = 50;
-
-            Hunter.Name = "Hunter";
-            Hunter.Type = "Craftra";
-            Hunter.Level = 1;
-            Hunter.CurrentExperiance = 0;
-            Hunter.ToNextExperiance = 1280;
-            Hunter.MaxLevel = 50;
-
-            FisherMan.Name = "Fisherman";
-            FisherMan.Type = "Craftra";
-            FisherMan.Level = 1;
-            FisherMan.CurrentExperiance = 0;
-            FisherMan.ToNextExperiance = 1280;
-            FisherMan.MaxLevel = 50;
-
-            Builder.Name = "Builder";
-            Builder.Type = "Craftra";
-            Builder.Level = 1;
-            Builder.CurrentExperiance = 0;
-            Builder.ToNextExperiance = 1280;
-            Builder.MaxLevel = 50;
-        }    
-    }
+    [Header("Leveling System")]
+    [Header("LumberJack")]
+    public int LumberJackLevel = 1;
+    public int LumberJackCurrentExp = 0;
+    public int LumberJackExpToNextLevel = 100;
+    [Header("Miner")]
+    public int MinerLevel = 1;
+    public int MinerCurrentExp = 0;
+    public int MinerExpToNextLevel = 100;
 
     // Start is called before the first frame update
     void Start()
